001260e6091581aeb7014adf0f7b3ba6ba60c347
martin <martin.lukacs@epitech.eu>
2016-08-02 19:34:10 +0200

    add navigation

---
+++ app/components/CountDown.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var CountDown = React.createClass({
+  render : function() {
+    return (
+      <div >
+        <p>CountDown.jsx</p>
+      </div>
+    );
+  }
+});
+
+module.exports = CountDown;
