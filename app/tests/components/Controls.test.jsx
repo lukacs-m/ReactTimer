efe7d3ce88357575ef36e241604103a7bdbbb882
martin <martin.lukacs@epitech.eu>
2016-08-06 19:41:35 +0200

    controls component functionnal

--- app/tests/components/Controls.test.jsx
+++ app/tests/components/Controls.test.jsx
@@ -12,13 +12,13 @@ describe('Controls', ()=> {
     });
 
     describe('render', () => {
-       it('should render pause when started', () => {
-          var controls = TestUtils.renderIntoDocument(<Controls countdownStatus="started"/>);
-           var $el = $(ReactDOM.findDOMNode(controls));
-           var $pauseButton = $el.find('button:contains(Pause)');
+        it('should render pause when started', () => {
+            var controls = TestUtils.renderIntoDocument(<Controls countdownStatus="started"/>);
+            var $el = $(ReactDOM.findDOMNode(controls));
+            var $pauseButton = $el.find('button:contains(Pause)');
 
-           expect($pauseButton.length).toBe(1);
-       });
+            expect($pauseButton.length).toBe(1);
+        });
 
         it('should render start when paused', () => {
             var controls = TestUtils.renderIntoDocument(<Controls countdownStatus="paused"/>);
@@ -28,26 +28,4 @@ describe('Controls', ()=> {
             expect($pauseButton.length).toBe(1);
         });
     });
-
-    // it('should call onSetCountdown if valid seconds entered', () => {
-    //     var spy = expect.createSpy();
-    //     var countdownForm = TestUtils.renderIntoDocument(<CountdownForm onSetCountdown={spy}/>);
-    //     var $el = $(ReactDOM.findDOMNode(countdownForm));
-    //
-    //     countdownForm.refs.seconds.value = "109";
-    //     TestUtils.Simulate.submit($el.find('form')[0]);
-    //
-    //     expect(spy).toHaveBeenCalledWith(109);
-    // });
-    //
-    // it('should not call onSetCountdown if invalid seconds entered', () => {
-    //     var spy = expect.createSpy();
-    //     var countdownForm = TestUtils.renderIntoDocument(<CountdownForm onSetCountdown={spy}/>);
-    //     var $el = $(ReactDOM.findDOMNode(countdownForm));
-    //
-    //     countdownForm.refs.seconds.value = "asdf";
-    //     TestUtils.Simulate.submit($el.find('form')[0]);
-    //
-    //     expect(spy).toNotHaveBeenCalled();
-    // });
 });
