49abb8d462946939496b659b381fe6706e343936
martin <martin.lukacs@epitech.eu>
2016-08-06 20:00:45 +0200

    correction of countdown little bug

--- app/components/CountDown.jsx
+++ app/components/CountDown.jsx
@@ -6,13 +6,13 @@ var Controls = require('Controls');
 var Countdown = React.createClass({
     getInitialState: function () {
         return {
-            count:0,
+            count: 0,
             countdownStatus: 'stopped'
         };
     },
     componentDidUpdate: function (prevProps, prevState) {
-        if(this.state.countdownStatus !== prevState.countdownStatus){
-            switch (this.state.countdownStatus){
+        if (this.state.countdownStatus !== prevState.countdownStatus) {
+            switch (this.state.countdownStatus) {
                 case 'started':
                     this.startTimer();
                     break;
@@ -25,17 +25,26 @@ var Countdown = React.createClass({
             }
         }
     },
+    componentWillUnmount: function () {
+        clearInterval(this.timer);
+        this.timer = undefined;
+    },
     startTimer: function () {
-      this.timer = setInterval(() => {
-          var newCount = this.state.count - 1;
-          this.setState({
-             count: newCount >=0 ? newCount : 0
-          });
-      }, 1000)
+        this.timer = setInterval(() => {
+            var newCount = this.state.count - 1;
+            this.setState({
+                count: newCount >= 0 ? newCount : 0
+            });
+
+            if (newCount === 0) {
+                this.setState({countdownStatus: 'stopped'});
+            }
+        }, 1000);
+
     },
     handleSetCountdown: function (seconds) {
         this.setState({
-            count:seconds,
+            count: seconds,
             countdownStatus: 'started'
         });
     },
@@ -45,7 +54,7 @@ var Countdown = React.createClass({
     render: function () {
         var {count, countdownStatus} = this.state;
         var renderControlArea = () => {
-            if (countdownStatus !== 'stopped'){
+            if (countdownStatus !== 'stopped') {
                 return <Controls countdownStatus={countdownStatus} onStatusChange={this.handleStatusChange}/>;
             } else {
                 return <CountdownForm onSetCountdown={this.handleSetCountdown}/>;
